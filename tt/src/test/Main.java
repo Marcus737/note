575cfaf5f607e5634f50c1ab6510a3c5cc78999d
文行 <1040****@qq.com>
2022-08-22 11:24:28 +0800

    123

--- tt/src/test/Main.java
+++ tt/src/test/Main.java
@@ -6,8 +6,10 @@ import java.net.Socket;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
+
 
     public static void main(String[] args) {
 
---
+++ tt/src/多线程/Main.java
@@ -0,0 +1,28 @@
+package 多线程;
+
+import java.util.concurrent.ExecutorService;
+
+class Worker implements Runnable{
+    @Override
+    public void run() {
+        synchronized (Worker.class){
+            for (int i = 1; i < 10; i++) {
+                System.out.println(Thread.currentThread().getName() + ":" + i);
+            }
+        }
+    }
+}
+
+public class Main {
+
+    public boolean ca(){
+        ExecutorService
+    }
+
+    public static void main(String[] args) {
+
+        Worker worker1 = new Worker(), worker2 = new Worker();
+        new Thread(worker1, "线程1").start();
+        new Thread(worker2, "线程2").start();
+    }
+}
